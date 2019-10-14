583f49aed6c8ecb4f9f82e31247f5f5c21e6b7eb
ivanvikhrev <l4ind@mail.ru>
2019-10-14 22:17:21 +0300

    delete extra files

--- src/GUI/ui/makedesign.bat
+++
@@ -1,4 +0,0 @@
-call pyuic5 StartWin.ui -o..\StartWin.py
-call pyuic5 AdminWin.ui -o..\AdminWin.py
-call pyuic5 ReaderWin.ui -o..\ReaderWin.py
-call pyuic5 BookWin.ui -o..\BookWin.py
--- src/modules/pvl/building.bat
+++
@@ -1,4 +0,0 @@
-mkdir build
-cd build
-cmake  -G"Visual Studio 15 2017 Win64" ../
-msbuild ALL_BUILD.vcxproj /p:Configuration=Release
