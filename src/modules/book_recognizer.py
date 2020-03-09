51b14ecbf1218af31d1fd735f45242f75d6b3cab
ivanvikhrev <l4ind@mail.ru>
2020-03-09 16:53:45 +0300

    add new versions of recognizers

--- src/modules/book_recognizer.py
+++ src/modules/book_recognizer.py
@@ -1,50 +1,27 @@
 import cv2
+import pyzbar.pyzbar as pyzbar
+import numpy as np
 from abc import ABC, abstractmethod
 
 class BookRecognizer(ABC):
+    @staticmethod
+    def create(args):
+        if args['name'] == 'QR':
+            return QRBookRecognizer()
+
     @abstractmethod
-    def create(self, detName):
-        """Create recognizer"""
-        
-    @abstractmethod
-    def recognize(self, frame, tpls, coeff):
+    def recognize(self, frame):
         """Recognize book"""
-        
 
-class Recognizer(BookRecognizer):
-    
-    def create(self, detName):
-        detectors = {"ORB":1, "SIFT":2, "SURF":3}
-        
-        try:
-            detectors[detName]
-            
-        except(KeyError):
-            print("Wrong detector name")
-            
-        else:
-            if (detName == "ORB"):
-                self.det = cv2.ORB_create() 
-            elif (detName == "SIFT"):
-                self.det = cv2.xfeatures2d.SIFT_create()
-            elif (detName == "SURF"):
-                self.det = cv2.xfeatures2d.SURF_create()
+class QRBookRecognizer(BookRecognizer):
+    def __init__(self):
+        self.objects = []
 
-            
-    def recognize(self, frame, desTpls, coeff):
-        arr = []
-        frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        _, desFrame = self.det.detectAndCompute(frameGray, None)
-
-        for t in desTpls:
-            matcher = cv2.BFMatcher()
-            matches = matcher.knnMatch(t, desFrame, k = 2)
-            good = []
-            
-            for m,n in matches:
-                if m.distance < n.distance * coeff: 
-                    good.append(m)
-                    
-            arr.append(len(good))
-        
-        return arr
+    def recognize(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        self.objects = pyzbar.decode(gray)
+        ans = ""
+        for obj in self.objects:
+            if obj.type == 'QRCODE':
+                ans = obj.data.decode('utf-8')
+        return ans
